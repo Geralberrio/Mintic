138d6a6ee1babaf50775b83afdb2b2b5ccabe8b3
Francocan <estiven_franco91152@elpoli.edu.co>
2021-09-23 15:39:35 -0500

    validaciones de formulario en JS

--- public_html/validaciones.js
+++ public_html/validaciones.js
@@ -3,16 +3,18 @@
     
 function validar_nombreUsuario(string){
            
-    var validar = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{4,9}$/;
-    var rta = validar.test(string);
+    //var validar = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{4,9}$/;
+    var validar1 = /\w{4,9}/;
+    var rta = validar1.test(string);
     
     return rta;
 }
 
 function validar_contrasena(string){
     
-    var validar = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
-    var rta = validar.test(string);
+    //var validar = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
+    var validar1 = /\w{6,}/;
+    var rta = validar1.test(string);
     
     return rta;
 }
