fa1956b0c448013ef368735b3d7e8cdd736b7fbf
Geralberrio <Geraldine.berrio12@gmail.com>
2021-09-29 14:01:25 -0500

    funciones sprint 3

--- public_html/validaciones.js
+++ public_html/validaciones.js
@@ -1,6 +1,4 @@
     
-    
-    
 function validar_nombreUsuario(string){
            
     //var validar = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{4,9}$/;
@@ -21,32 +19,3 @@ function validar_contrasena(string){
 
 module.exports.validar_nombreUsuario = validar_nombreUsuario;
 module.exports.validar_contrasena = validar_contrasena;
-
-let registros = [];
-
-function agregarRegistro(){
-    let form = document.form_registro_usuario;
-    registros.push([form.in_usuario.value,form.in_contrasena.value]);
-}
-
-function filtrarPorContrasena(arreglo,filtro){
-    arreglo_validador =[];
-    for (var i=0; i < arreglo.length; i++){
-        if (arreglo[i][0].length <= filtro) {
-            arreglo_validador.push(arreglo[i]);
-        }
-    }
-    console.log(arreglo_validador);
-    return arreglo_validador;
-}
-
-
-module.exports.registros = registros;
-module.exports.agregarRegistro = agregarRegistro;
-module.exports.filtrarPorContrasena = filtrarPorContrasena;
-
-
-
-
-
-
