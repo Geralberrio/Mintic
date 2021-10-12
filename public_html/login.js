bc63cf760215e1def09014ae222168ae26c49014
Geralberrio <Geraldine.berrio12@gmail.com>
2021-10-12 11:06:19 -0500

    cambios sprint 5

---
+++ public_html/login.js
@@ -0,0 +1,43 @@
+let registros = [];
+
+
+function agregarRegistro(){
+    function registro(usuario,contrasena){
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+    
+    var capturarUsuario = document.getElementById("in_usuario").value;
+    var capturarContrasena = document.getElementById("in_contrasena").value;
+    var nuevoRegistro = new registro(capturarUsuario,capturarContrasena);
+    registros.push(nuevoRegistro);
+    console.log(registros);
+}
+
+function validar_captcha(rcaptcha){
+    let validar = false;
+    if (rcaptcha === 5){
+        validar = true;
+    }
+    return validar;
+}
+
+function iniciar_sesion(usuario = document.document.getElementById("login_usuario").value, 
+                        contrasena = document.document.getElementById("login_contrasena").value,
+                        rcaptcha=document.document.getElementById("login_captcha").value){
+   var prueba = false;
+   for (var i=0; i < registros.length; i++){
+        if (registros[i]['usuario']=== usuario & 
+            registros[i]['contrasena']===contrasena & 
+            validar_captcha(rcaptcha)) {
+            prueba = true;
+        }
+    }
+    return prueba;
+  
+}
+
+module.exports.registros = registros;
+module.exports.agregarRegistro = agregarRegistro;
+module.exports.iniciar_sesion = iniciar_sesion;
+module.exports.validar_captcha = validar_captcha;
--- public_html/registro.js
+++ public_html/registro.js
@@ -1,14 +1,5 @@
 
 let registros = [];
-/*
-function agregarRegistro(){
-    let registro_unit = [];
-    var usuario_form = document.getElementById("in_usuario").value;
-    var contrasena_form = document.getElementById("in_contrasena").value;
-    registro_unit['usario']= usuario_form;
-    registro_unit['contrasena'] = contrasena_form;
-    registros.push(registro_unit);
-}*/
 
 function agregarRegistro(){
     function registro(usuario,contrasena){
